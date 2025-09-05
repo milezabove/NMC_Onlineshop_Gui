5b6e4dae8230f118a9b367e2185779cb6b44be78
Milena Mathys <mma142246@stud.gibb.ch>
2025-09-05 22:57:52 +0200

    add prod api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://onlineshop-api-d8cpfzcmg0e0gxg4.eastus-01.azurewebsites.net/api'
+  apiBaseUrl: 'https://onlineshop-api-d8cpfzcmg0e0gxg4.canadacentral-01.azurewebsites.net/api'
 };
